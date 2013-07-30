6e4463a8146a1d06ca9fe48ed9231d8391a6f883
Alex Roth <science9711@gmail.com>
2013-07-30 13:25:57 -0400

    Removed need to rechange file extension

--- Web_HTML/upload_audio.php
+++ Web_HTML/upload_audio.php
@@ -3,7 +3,7 @@
 if($_FILES['upFile']['type'] == "audio/amr" || $_FILES['upFile']['type'] == "audio/wav"){
 $uploaddir = "upload_audio/";
 $uuid = uniqid();
-$uploadfile = $uploaddir . basename( $_FILES['upFile']['name']). "-".$uuid;
+$uploadfile = $uploaddir .$uuid."-".basename( $_FILES['upFile']['name'] );
 if(move_uploaded_file($_FILES['upFile']['tmp_name'], $uploadfile))
 {
   echo $uuid;
--- Web_HTML/upload_video.php
+++ Web_HTML/upload_video.php
@@ -3,7 +3,7 @@
 if($_FILES['upFile']['type'] == "video/3gpp" || $_FILES['upFile']['type'] == "video/quicktime"||$_FILES['upFile']['type'] == "video/mp4"){
 $uploaddir = "upload_video/";
 $uuid = uniqid();
-$uploadfile = $uploaddir . basename( $_FILES['upFile']['name'] ). "-".$uuid;
+$uploadfile = $uploaddir .$uuid."-".basename( $_FILES['upFile']['name'] );
 if(move_uploaded_file($_FILES['upFile']['tmp_name'], $uploadfile))
 {
   echo $uuid;
