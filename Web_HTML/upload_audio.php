4190682a20347d7ae3d8b05cae965a4b441cebee
Alex Roth <science9711@gmail.com>
2013-07-30 12:55:51 -0400

    Modified upload_audio to bring it into line with video

--- Web_HTML/upload_audio.php
+++ Web_HTML/upload_audio.php
@@ -7,9 +7,5 @@ if(move_uploaded_file($_FILES['upFile']['tmp_name'], $uploadfile))
 {
   echo "The file has been uploaded successfully";
 }
-else
-{
-  echo "There was an error uploading the file";
-}
 }
 ?>
