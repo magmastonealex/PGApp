45999ef69679f5ded366e281a4447dea521e8184
Alex Roth <science9711@gmail.com>
2013-07-30 13:08:41 -0400

    Returning the UUID so the app can put it in the database properly.

--- Web_HTML/upload_audio.php
+++ Web_HTML/upload_audio.php
@@ -2,10 +2,11 @@
 
 if($_FILES['upFile']['type'] == "audio/amr" || $_FILES['upFile']['type'] == "audio/wav"){
 $uploaddir = "upload_audio/";
-$uploadfile = $uploaddir . basename( $_FILES['upFile']['name']). "-".uniqid();
+$uuid = uniqid();
+$uploadfile = $uploaddir . basename( $_FILES['upFile']['name']). "-".$uuid;
 if(move_uploaded_file($_FILES['upFile']['tmp_name'], $uploadfile))
 {
-  echo "The file has been uploaded successfully";
+  echo $uuid;
 }
 }
 ?>
--- Web_HTML/upload_video.php
+++ Web_HTML/upload_video.php
@@ -2,10 +2,11 @@
 
 if($_FILES['upFile']['type'] == "video/3gpp" || $_FILES['upFile']['type'] == "video/quicktime"||$_FILES['upFile']['type'] == "video/mp4"){
 $uploaddir = "upload_video/";
-$uploadfile = $uploaddir . basename( $_FILES['upFile']['name'] ). "-".uniqid();
+$uuid = uniqid();
+$uploadfile = $uploaddir . basename( $_FILES['upFile']['name'] ). "-".$uuid;
 if(move_uploaded_file($_FILES['upFile']['tmp_name'], $uploadfile))
 {
-  echo "The file has been uploaded successfully";
+  echo $uuid;
 }
 }
 ?>
