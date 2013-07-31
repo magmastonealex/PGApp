6f94b6b39c6f1ba0bace400f724f83254387387c
Alex Roth <science9711@gmail.com>
2013-07-31 12:30:27 -0400

    Fixed a typo in getting form fields.

--- Web_HTML/get_form_field.php
+++ Web_HTML/get_form_field.php
@@ -2,7 +2,7 @@
 $db = new mysqli('localhost', 'appd2dpr_php', '9&3mPMjCJM8+uKy6o', 'appd2dpr_mobileApp');
 
 $query = $db->prepare("SELECT * FROM form_fields WHERE formID=? ORDER BY fldOrder ASC;");
-$formID=2
+$formID=2;
 $query->bind_param('i', $formid);
 $query->execute();
 $query->bind_result($fldID, $formID, $fldType, $fieldOptions, $fldOrder, $fldStatus);
