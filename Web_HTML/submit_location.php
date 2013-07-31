eebe20eb28e466d252eb9ede67fccebb7430d348
Alex Roth <science9711@gmail.com>
2013-07-31 17:54:19 -0400

    Fixed an SQL prepared statement.

--- Web_HTML/submit_location.php
+++ Web_HTML/submit_location.php
@@ -2,13 +2,14 @@
 $db = new mysqli('localhost', 'appd2dpr_php', '9&3mPMjCJM8+uKy6o', 'appd2dpr_mobileApp');
 
 
-$prepstate = $db->prepare('INSERT INTO tracker_table VALUES (NULL,NULL,?,?,?,?,?,1)');
+$prepstate = $db->prepare('INSERT INTO tracker_table VALUES (NULL,NULL,?,?,?,?,?,?)');
    $userid = $db->real_escape_string($_POST['userID']);
    $devid = $db->real_escape_string($_POST['deviceID']);
    $interval= $db->real_escape_string($_POST['interval']);
    $lat = $db->real_escape_string($_POST['latitude']);
    $longit = $db->real_escape_string($_POST['longitude']);
-   $prepstate->bind_param("sssss", $devid,$userid,$interval,$lat,$longitude);
+   $status="false"
+   $prepstate->bind_param("sssss", $devid,$userid,$interval,$lat,$longitude,$status);
    if(!$prepstate->execute()){
    	  die('Error: ' + $db->error);
    }
