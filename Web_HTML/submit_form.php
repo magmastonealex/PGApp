fc512b786bb26a09686b80dd6b4c09caa08497d8
Alex Roth <science9711@gmail.com>
2013-07-31 11:57:30 -0400

    Added the ability to turn geolocation off. Also added a test parser for uploaded form data.

--- Web_HTML/submit_form.php
+++ Web_HTML/submit_form.php
@@ -1,17 +1,3 @@
 <?php
-$db = new mysqli('localhost', 'appd2dpr_php', '9&3mPMjCJM8+uKy6o', 'appd2dpr_mobileApp');
-
-if($db->connect_errno > 0){
-    die('Error :  ' . $db->connect_error);
-}
-
-$query = $db->prepare("INSERT INTO formsubmit_temp VALUES (?,?,?,?,?);");
-$id = uniqid();
-$formid = 44932;
-$deviceid= 44422;
-$userid=44293923;
-$form_data="Test Data";
-
-$query->bind_param('iiiis', $id, $formid, $deviceid, $userid, $form_data);
-$query->execute();
+file_put_contents("Test_Input", $_POST["formsubmission"];)
 ?>
