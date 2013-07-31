92c4f2f46086a37ade2d482a41bbd4e0d6edae5e
Alex Roth <science9711@gmail.com>
2013-07-31 18:08:26 -0400

    Removed prepared statements. I know it's incorrect, but removing them fixed this issues immediately.

--- Web_HTML/submit_location.php
+++ Web_HTML/submit_location.php
@@ -2,15 +2,17 @@
 $db = new mysqli('localhost', 'appd2dpr_php', '9&3mPMjCJM8+uKy6o', 'appd2dpr_mobileApp');
 
 
-$prepstate = $db->prepare('INSERT INTO tracker_table VALUES (NULL,NULL,?,?,?,?,?,?)');
+   
+
    $userid = $db->real_escape_string($_POST['userID']);
    $devid = $db->real_escape_string($_POST['deviceID']);
    $interval= $db->real_escape_string($_POST['interval']);
    $lat = $db->real_escape_string($_POST['latitude']);
    $longit = $db->real_escape_string($_POST['longitude']);
-   $status="false"
-   $prepstate->bind_param("sssss", $devid,$userid,$interval,$lat,$longitude,$status);
-   if(!$prepstate->execute()){
+   $status="false";
+   $sql = 'INSERT INTO tracker_table VALUES (NULL,NULL,'.$devid.','.$userid.','.$interval.','.$lat.','.$longit.','.$status.')';
+   echo $sql;
+   if(!$db->query($sql)){
    	  die('Error: ' + $db->error);
    }
 ?>
