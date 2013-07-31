c7b131bdeb2406ae0cafc75b2b89e0a250487dac
Alex Roth <science9711@gmail.com>
2013-07-31 10:37:42 -0400

    Added datables access

--- Web_HTML/submit_form.php
+++ Web_HTML/submit_form.php
@@ -1,5 +1,11 @@
 <?php
+$db = new mysqli('localhost', 'appd2dpr_php', '9&3mPMjCJM8+uKy6o', 'appd2dpr_mobileApp');
 
-file_put_contents("Results", $_POST["formsubmission"]);
-
+if($db->connect_errno > 0){
+    die('Error :  ' . $db->connect_error);
+}
+$query = "INSERT INTO devices VALUES (4901313441, 994832,99223344)";
+if(!$result=$db->query($query)){
+    die('Insert error: '.$db->error);
+}
 ?>
