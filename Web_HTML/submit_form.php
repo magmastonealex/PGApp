e3c98e15d0ea752e628e53dd9c34fca00a7328c3
Alex Roth <science9711@gmail.com>
2013-07-31 10:45:02 -0400

    Added update functionality.

--- Web_HTML/submit_form.php
+++ Web_HTML/submit_form.php
@@ -4,8 +4,11 @@ $db = new mysqli('localhost', 'appd2dpr_php', '9&3mPMjCJM8+uKy6o', 'appd2dpr_mob
 if($db->connect_errno > 0){
     die('Error :  ' . $db->connect_error);
 }
-$query = "INSERT INTO devices VALUES (4901313441, 994832,99223344)";
+$query = "INSERT INTO devices VALUES (4901313441, 994832,99223344);";
 if(!$result=$db->query($query)){
-    die('Insert error: '.$db->error);
+    $query = "UPDATE devices SET userid=994832,timestamp=99253344 WHERE deviceid=4901313441;"
+    if(!$result=$db->query($query)){
+    	die("Error updating:" . $db->error);
+	}
 }
 ?>
