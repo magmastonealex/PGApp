43633c49ca5e2a64bf85cd5af78e8c2875382c5d
Alex Roth <science9711@gmail.com>
2013-07-31 16:08:52 -0400

    Get rid of all of the \ in the returned json.

--- Web_HTML/submit_form.php
+++ Web_HTML/submit_form.php
@@ -1,7 +1,7 @@
 <?php
 $db = new mysqli('localhost', 'appd2dpr_php', '9&3mPMjCJM8+uKy6o', 'appd2dpr_mobileApp');
 $injson_proc = str_replace("\\", "", $_POST["formsubmission"]);
-$injson = json_decode();
+$injson = json_decode($injson_proc);
 
 
 if($db->connect_errno > 0){
