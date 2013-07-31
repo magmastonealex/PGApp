b3725510a738279187b998846b4dd0485e1da01a
Alex Roth <science9711@gmail.com>
2013-07-31 12:43:49 -0400

    Fixed a typo

--- Web_HTML/get_form_field.php
+++ Web_HTML/get_form_field.php
@@ -4,7 +4,7 @@ $db = new mysqli('localhost', 'appd2dpr_php', '9&3mPMjCJM8+uKy6o', 'appd2dpr_mob
 $query = "SELECT * FROM form_fields WHERE formID=2 ORDER BY fldOrder ASC;";
 $formID=2;
 
-if(!$result = $db->query($sql)){
+if(!$result = $db->query($query)){
     die('Error: ' . $db->error . ']');
 }
 echo "Here";
