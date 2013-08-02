6ab687b4d08b72fb4764f7d3e4b6e552d807f49e
Alex Roth <sceicen9711@gmail.com>
2013-08-01 20:48:24 -0400

    Create header before going into JSON return function.

--- www/js/index.js
+++ www/js/index.js
@@ -90,11 +90,11 @@ function formEntryHandler(){
 function formDetailHandle(){
     $('.formDDetail').on("tap",function(){
         console.log($(this).attr("subID"));
-        $.ajaxSetup({async: false, error: function(error){alert("Error downloading Detail");}});
-
-        $.getJSON("http://app.d2dpro.com/view_result.php", {"subID":$(this).attr("subID")}).done(function(data){
+        $.ajaxSetup({async: true, error: function(error){alert("Error downloading Detail");}});
             $("#entries_super_detail_header").html("<h1>"+$(this).html()+"</h1>");
             $("#entries_super_detail").trigger("create");
+        $.getJSON("http://app.d2dpro.com/view_result.php", {"subID":$(this).attr("subID")}).done(function(data){
+            
             console.log(JSON.stringify(data));
             $("#entries_super_detail_content").html("");
             for(var iter=0; iter<data.length;iter++){
